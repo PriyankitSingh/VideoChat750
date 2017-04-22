840ef239055166f6cc12e81cee611a979e3ddbb9
Mepivo <noemail@example.com>
2017-04-22 18:04:38 +1200

    Refratored and removed unused code

--- video.js
+++ video.js
@@ -1,44 +1,74 @@
 var video_out = document.getElementById("vid-box");
 var vid_thumb = document.getElementById("vid-thumb");
 var vidCount = 0;
+var bandwidth = 250;
+
+function setBandwidth(form){
+		bandwidth = form.bandwidth.value;
+		return false;
+}
+
 function login(form) {
-	var phone = window.phone = PHONE({
-	    number        : form.username.value || "Anonymous", 
-		publish_key   : 'pub-c-561a7378-fa06-4c50-a331-5c0056d0163c', // Your Pub Key
+
+	var phone = window.phone =
+	PHONE({
+	    number        : form.username.value || "Anonymous",
+			publish_key   : 'pub-c-561a7378-fa06-4c50-a331-5c0056d0163c', // Your Pub Key
 	    subscribe_key : 'sub-c-17b7db8a-3915-11e4-9868-02ee2ddab7fe', // Your Sub Key
+			media : {audio :true, video: true}
 	    //publish_key   : 'pub-c-4972d566-854b-41ef-9f97-25d40f968e28',
 	    //subscribe_key : 'sub-c-0369f0f0-0bc7-11e7-9734-02ee2ddab7fe',
-	});	
-	
+	});
+
 	var ctrl = window.ctrl = CONTROLLER(phone, get_xirsys_servers);
 	ctrl.ready(function(){
-		form.username.style.background="#55ff5b"; 
-		form.login_submit.hidden="true"; 
+		form.username.style.background="#55ff5b";
+		form.login_submit.hidden="true";
+
+		//Here we possibly want to minimise the user's screen
 		ctrl.addLocalStream(vid_thumb);
-		addLog("Logged in as " + form.username.value); 
+		//addLog("Logged in as " + form.username.value);
+		console.log("Logged in as " + form.username.value);
 	});
 	ctrl.receive(function(session){
-	    session.connected(function(session){ video_out.appendChild(session.video); addLog(session.number + " has joined."); vidCount++; });
-	    session.ended(function(session) { ctrl.getVideoElement(session.number).remove(); addLog(session.number + " has left.");    vidCount--;});
+	    session.connected(function(session){ video_out.appendChild(session.video);
+
+																					 //Adding button for kicking a session
+																					 //var kickbtn = document.createElement("button");
+																					 //video_out.appendChild(kickbtn);
+				 																	 //addLog(session.number + " has joined.");
+																					 console.log(session.number + " has joined.");
+																					 vidCount++; });
+	    session.ended(function(session) { ctrl.getVideoElement(session.number).remove();
+																				//addLog(session.number + " has left.");
+																				console.log(session.number + " has left.");
+																			 	vidCount--;});
 	});
+
 	ctrl.videoToggled(function(session, isEnabled){
 		ctrl.getVideoElement(session.number).toggle(isEnabled);
-		addLog(session.number+": video enabled - " + isEnabled);
+		//addLog(session.number+": video enabled - " + isEnabled);
+		console.log(session.number+": video enabled - " + isEnabled);
 	});
 	ctrl.audioToggled(function(session, isEnabled){
 		ctrl.getVideoElement(session.number).css("opacity",isEnabled ? 1 : 0.75);
-		addLog(session.number+": audio enabled - " + isEnabled);
+		//addLog(session.number+": audio enabled - " + isEnabled);
+		console.log(session.number+": audio enabled - " + isEnabled);
 	});
 	return false;
 }
+
 function makeCall(form){
 	if (!window.phone) alert("Login First!");
 	var num = form.number.value;
 	if (phone.number()==num) return false; // No calling yourself!
-	ctrl.isOnline(num, function(isOn){
-		if (isOn) ctrl.dial(num);
-		else alert("User if Offline");
-	});
+
+	ctrl.isOnline(num,
+		function(isOn){
+			if (isOn) ctrl.dial(num);
+			else alert("User if Offline");
+		}
+	);
 	return false;
 }
 
@@ -54,19 +84,19 @@ function end(){
 
 function pause(){
 	var video = ctrl.toggleVideo();
-	if (!video) $('#pause').html('Unpause'); 
-	else $('#pause').html('Pause'); 
+	if (!video) $('#pause').html('Unpause');
+	else $('#pause').html('Pause');
 }
 
 function getVideo(number){
 	return $('*[data-number="'+number+'"]');
 }
+//
+// function addLog(log){
+// 	$('#logs').append("<p>"+log+"</p>");
+// }
 
-function addLog(log){
-	$('#logs').append("<p>"+log+"</p>");
-}
-
-function get_xirsys_servers() {
+	function get_xirsys_servers() {
     var servers;
     $.ajax({
         type: 'POST',
