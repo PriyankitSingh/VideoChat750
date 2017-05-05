f7246bbabf53bd567da658919f72dcae3dca8c0b
Formene <examplt@noemail.com>
2017-05-05 13:11:34 +1200

    moved bandwidth stuff to a new file

--- video.js
+++ video.js
@@ -28,45 +28,6 @@ var participantBandwidths = [];
 var phone;
 var chatlogs = document.getElementById('chatlogs');
 
-// This function measures the video availableBandwidth
-function invokeGetStats(peerConnection){
-	getStats(peerConnection, function(result ) {
-		participantBandwidths[peerConnection.number]=  result.video.availableBandwidth;
-		console.log(peerConnection.number +": " +participantBandwidths[peerConnection.number]);
-		if (result.datachannel && result.datachannel.state === 'close') {
-			delete dic[peerConnection.number];
-			console.log("removed" + peerConnection.number);
-			result.nomore();
-		}
-		window.getStatsResult = result;
-	}, 5 * 1000);
-
-}
-
-
-function setBandwidth(form){
-	bandwidth = form.bandwidth.value;
-	if(bandwidth.toLowerCase() == "high"){
-		if(send_loop_id == null){
-			//do nothing
-		}else{
-			toggle_to_high();
-			send_toggle_message("high");
-		}
-		
-	}else if(bandwidth.toLowerCase() == "low"){
-		if(send_loop_id == null){
-			toggle_to_low();
-			send_toggle_message("low");
-		}else{
-			//do nothing
-		}
-	}else{
-		alert("Only High or Low accepted not: "  + bandwidth);
-	}
-	return false;
-}
-
 function login(form) {
 
 	phone = window.phone =
